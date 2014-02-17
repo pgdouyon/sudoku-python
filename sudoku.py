87b5493d4e6547e0fa4f05a28ebaeb7238a7d7ce
pgdouyon <pgdouyon@alum.mit.edu>
2014-02-17 07:13:05 -0500

    Complete singleton propagation solution
    Singleton propagation is fully implemented and tested.  Script can solve
    "easy" sudoku puzzles that do not require dfs.

--- sudoku.py
+++ sudoku.py
@@ -4,48 +4,52 @@ import itertools
 
 
 class MinHeap(object):
-    """Docstring for MinHeap """
+    """Priority Queue used to keep track of GridEntry
+    objects with lowest number of candidates"""
 
     def __init__(self):
-        """@todo: to be defined """
         self.heap = [None]
         self.heap_size = 0
 
     @staticmethod
     def parent(index):
-        """@todo: Docstring for parent
+        """
+        Return heap-index for the parent of node at index
 
-        :index: @todo
-        :returns: @todo
+        :index: index of child node
+        :returns: index of parent node
 
         """
         return index // 2
 
     @staticmethod
     def left_child(index):
-        """@todo: Docstring for left_child
+        """
+        Return heap-index for the left-child of node at index
 
-        :index: @todo
-        :returns: @todo
+        :index: index of parent node
+        :returns: index of left-child
 
         """
         return index << 1
 
     @staticmethod
     def right_child(index):
-        """@todo: Docstring for right_child
+        """
+        Return the heap-index for the right-child of node at index
 
-        :index: @todo
-        :returns: @todo
+        :index: index of parent node
+        :returns: index of right-child
 
         """
         return (index << 1) + 1
 
     def min_heapify(self, i):
-        """@todo: Docstring for min_heapify
+        """
+        Build a min-heap out of node i and the two min-heaps rooted at
+        Left(i) and Right(i)
 
-        :i: @todo
-        :returns: @todo
+        :i: index of root node
 
         """
         l = MinHeap.left_child(i)
@@ -61,25 +65,46 @@ class MinHeap(object):
             self.min_heapify(min_i)
 
     def build_min_heap(self, arr):
-        """@todo: Docstring for build_min_heap
+        """
+        Build min-heap from input array
 
-        :arr: @todo
-        :returns: @todo
+        :arr: array to be heapified
 
         """
         size = len(arr)
         self.heap = [None] + arr
         self.heap_size = size
+        self.rebuild_heap()
+
+    def rebuild_heap(self):
+        """
+        Rebuild min-heap from self.heap.  Used when heap values have mutated
+        and heap invariant must be restored throughout heap.
+
+        """
+        size = self.heap_size
         for i in range(size // 2, 0, -1):
             self.min_heapify(i)
 
     def get_min(self):
+        """
+        Return the node at the top of the heap, but do not remove it.
+
+        :returns: Node with minimum size
+
+        """
         if self.heap_size < 1:
             return None
         else:
             return self.heap[1]
 
     def extract_min(self):
+        """
+        Remove and return the node at the top of the heap.
+
+        :returns: Node with minimum size
+
+        """
         if self.heap_size < 1:
             return None
         min_node = self.heap[1]
@@ -88,88 +113,111 @@ class MinHeap(object):
         self.min_heapify(1)
         return min_node
 
+    def get_heap(self):
+        """
+        Return array representation of heap.
+
+        :returns: array representation of heap
+
+        """
+        return self.heap[1:]
+
 
 class GridEntry(object):
-    """Docstring for GridEntry """
+    """
+    Stores the coordinates, value, and potential candidates for
+    sudoku grid cells"""
 
     def __init__(self, x, y, value):
-        """@todo: to be defined
+        """Initializes coordinates, value, and empty candidates set.
 
-        :x: @todo
-        :y: @todo
-        :value: @todo
+        :x: x-coordinate
+        :y: y-coordinate
+        :value: initial value
 
         """
         self.x = x
         self.y = y
         self.value = value
-        self.candidates = []
+        self.candidates = set([])
 
     def get_x(self):
-        """@todo: Docstring for get_x
-        :returns: @todo
+        """
+        Return x-coordinate
+
+        :returns: x-coordinate
 
         """
         return self.x
 
     def get_y(self):
-        """@todo: Docstring for get_y
-        :returns: @todo
+        """
+        Return y-coordinate
+
+        :returns: y-coordinate
 
         """
         return self.y
 
     def get_value(self):
-        """@todo: Docstring for get_value
-        :returns: @todo
+        """
+        Return value of cell
+
+        :returns: value
 
         """
         return self.value
 
     def get_candidates(self):
-        """@todo: Docstring for get_candidates
-        :returns: @todo
+        """
+        Return set of candidates
+
+        :returns: set of candidates
 
         """
         return self.candidates
 
     def set_value(self, val):
-        """@todo: Docstring for set_value.
+        """
+        Set cell value
 
-        :val: @todo
-        :returns: @todo
+        :val: value to be set
 
         """
         self.value = val
 
     def set_candidates(self, cands):
-        """@todo: Docstring for set_candidates.
+        """
+        Set candidates set
 
-        :cands: @todo
-        :returns: @todo
+        :cands: candidates set
 
         """
         self.candidates = cands
 
     def size(self):
-        """@todo: Docstring for size
-        :returns: @todo
+        """
+        Return length of candidate set for this cell
+
+        :returns: length of candidate set
 
         """
         return len(self.candidates)
 
     def has_value(self):
-        """@todo: Docstring for has_value.
-        :returns: @todo
+        """
+        Check if cell has a value assigned
+        :returns: true if cell's value has been set
 
         """
         return self.value != "_"
 
     def __lt__(self, entry):
-        """@todo: Docstring for __lt__
+        """
+        Override __lt__ method.  Compare cells by length of candidate sets.
 
-        :entry: @todo
-        :returns: @todo
+        :entry: cell to be compared against
+        :returns: true if this cell has a smaller candidate set
 
         """
         if not isinstance(entry, GridEntry):
@@ -177,10 +225,11 @@ class GridEntry(object):
         return len(self.candidates) < len(entry.candidates)
 
     def __eq__(self, entry):
-        """@todo: Docstring for __eq__
+        """
+        Override __eq__ method.  Compare cells by length of candidate sets.
 
-        :entry: @todo
-        :returns: @todo
+        :entry: cell to be compared against
+        :returns: true if this cell has an equal sized candidate set
 
         """
         if not isinstance(entry, GridEntry):
@@ -189,10 +238,19 @@ class GridEntry(object):
 
 
 def make_grid(filename):
-    """@todo: Docstring for make_grid
+    """
+    Build a 2D square array from txt file.  File should be a comma-separated
+    list of values representing a sudoku grid.  Blank cells should be
+    represented by a "_"
 
-    :filename: @todo
-    :returns: @todo
+    i.e.
+
+    1,_,3
+    2,3,_
+    _,_,2
+
+    :filename: name of file storing sudoku grid values
+    :returns: 2D square array representing sudoku grid
 
     """
     arr = [line.strip().split(',') for line in open(filename)]
@@ -204,11 +262,12 @@ def make_grid(filename):
 
 
 def get_row(grid, entry):
-    """@todo: Docstring for get_row
+    """
+    Return list of GridEntry objects located in the row containing 'entry'
 
-    :grid: @todo
-    :entry: @todo
-    :returns: @todo
+    :grid: 2D array representing sudoku grid
+    :entry: element of grid array
+    :returns: list of GridEntry objects in the same row as 'entry'
 
     """
     x = entry.get_x()
@@ -216,11 +275,12 @@ def get_row(grid, entry):
 
 
 def get_col(grid, entry):
-    """@todo: Docstring for get_col
+    """
+    Return list of GridEntry objects located in the col containing 'entry'
 
-    :grid: @todo
-    :entry: @todo
-    :returns: @todo
+    :grid: 2D array representing sudoku grid
+    :entry: element of grid array
+    :returns: list of GridEntry objects in the same col as 'entry'
 
     """
     y = entry.get_y()
@@ -229,11 +289,12 @@ def get_col(grid, entry):
 
 
 def get_box(grid, entry):
-    """@todo: Docstring for get_box
+    """
+    Return list of GridEntry objects located in the box containing 'entry'
 
-    :grid: @todo
-    :entry: @todo
-    :returns: @todo
+    :grid: 2D array representing sudoku grid
+    :entry: element of grid array
+    :returns: list of GridEntry objects in the same box as 'entry'
 
     """
     x = entry.get_x()
@@ -247,10 +308,11 @@ def get_box(grid, entry):
 
 
 def make_value_set(entries):
-    """@todo: Docstring for make_value_set.
+    """
+    Build a set of all the entry values stored in argument list
 
-    :entries: @todo
-    :returns: @todo
+    :entries: list of GridEntry objects
+    :returns: set of values stored in 'entries'
 
     """
     val_set = set([e.get_value() for e in entries if e.has_value()])
@@ -258,73 +320,99 @@ def make_value_set(entries):
 
 
 def filter_initial_values(flatgrid):
-    """@todo: Docstring for filter_initial_candidates.
+    """
+    Remove GridEntry objects that already have a value set.
+    The list returned from this method will be used to
+    populate the min-heap with all unknown cells whose
+    values still need to be determined.
 
-    :flatgrid: @todo
-    :returns: @todo
+    :flatgrid: 1D array of a flattend 2D sudoku grid
+    :returns: list of GridEntry objects with unknown/unset values
 
     """
-    filtered = filter(lambda e: not e.has_value(), flatgrid)
+    filtered = [e for e in flatgrid if not e.has_value()]
     return filtered
 
 
-def initialize_candidates(grid):
-    """@todo: Docstring for update_candidates
+def update_candidates(grid, entries):
+    """
+    Recalculates possible candidates for each GridEntry object in entries.
+    Used after grid values have been updated.
 
-    :grid: @todo
-    :returns: @todo
+    :grid: 2D sudoku grid
+    :entries: 1D array of flattened 2D sudoku grid
 
     """
-    flat_grid = list(itertools.chain.from_iterable(grid))
-    filtered = filter_initial_values(flat_grid)
-    for entry in filtered:
+    for entry in entries:
         row_set = make_value_set(get_row(grid, entry))
         col_set = make_value_set(get_col(grid, entry))
         box_set = make_value_set(get_box(grid, entry))
         val_set = row_set.union(col_set.union(box_set))
-        all_vals = set(map(str, range(1, len(grid) + 1)))
-        candidates = all_vals.difference(val_set)
+        all_nums = set(map(str, range(1, len(grid) + 1)))
+        candidates = all_nums.difference(val_set)
         entry.set_candidates(candidates)
+
+
+def initialize_candidates(grid):
+    """
+    Populate GridEntry objects with unknown values with a set of all
+    possible candidate values.
+
+    :grid: 2D sudoku grid
+    :returns: 1D list of unknown GridEntry objects with candidates updated
+
+    """
+    flat_grid = list(itertools.chain.from_iterable(grid))
+    filtered = filter_initial_values(flat_grid)
+    update_candidates(grid, filtered)
     return filtered
 
 
-def propagate_singletons(grid, heap):
-    """@todo: Docstring for propagate_singletons.
+def propagate_all_singletons(grid, heap):
+    """
+    Recursively checks the top element of the heap.  If it is a singleton,
+    meaning it has only one candidate value, then that value must be the value
+    of the cell.  That value is set and the entry is removed from the heap
+    since its value has been determined.
 
-    :grid: @todo
-    :heap: @todo
-    :returns: @todo
+    Updates the candidates of all objects remaining in the heap and rebuilds
+    the heap when all known singletons have been removed.
+
+    :grid: 2D sudoku grid
+    :heap: min-heap of unknown GridEntry objects
 
     """
     while heap.get_min() and heap.get_min().size() == 1:
         singleton = heap.extract_min()
         val = singleton.get_candidates().pop()
         singleton.set_value(val)
+    update_candidates(grid, heap.get_heap())
+    heap.rebuild_heap()
 
-def update_grid_entry(grid, entry):
-    """@todo: Docstring for update_grid_entry.
-
-    :grid: @todo
-    :entry: @todo
-    :returns: @todo
-
-    """
-    row = get_row(grid, entry)
-    col = get_col(grid, entry)
-    box = get_box(grid, entry)
 
 def solve_sudoku(filename):
-    """@todo: Docstring for solve_sudoku
+    """
+    Builds the 2D grid representation of sudoku puzzle and solves it via a
+    combination of singleton propagation and depth-first search.
 
-    :filename: @todo
-    :returns: @todo
+    :filename: file containing sudoku grid
 
     """
     sudoku_grid = make_grid(filename)
     filtered_grid = initialize_candidates(sudoku_grid)
     heap = MinHeap()
     heap.build_min_heap(filtered_grid)
-    propagate_singletons(sudoku_grid, heap)
+
+    while heap.get_min() and heap.get_min().size() == 1:
+        propagate_all_singletons(sudoku_grid, heap)
+
+
+def print_sudoku(grid):
+    for row in grid:
+        s = ""
+        for entry in row:
+            s = s + entry.get_value() + ","
+        print(s)
 
 if __name__ == "__main__":
     print("Main")
